445ba3fd2b075542a254668c0beb7dc0b04bed97
Rafael <mvr.h.morales24@gmail.com>
2026-04-23 19:45:07 -0400

    ratefinder first version

---
+++ main.py
@@ -0,0 +1,30 @@
+﻿from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+with open("rates.json", "r", encoding="utf-8-sig") as f:
+    data = json.load(f)
+
+@app.get("/rate")
+def get_rate(job: str, level: str, country: str):
+    item = data[job][level][country]
+
+    return {
+        "job": job,
+        "level": level,
+        "country": country,
+        "min": item["min"],
+        "max": item["max"],
+        "range": f"{item['min']} - {item['max']}",
+        "source": item["source"]
+    }
